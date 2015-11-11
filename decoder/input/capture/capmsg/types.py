c5b8895f19cc385fd7326bb3513fd84c09e508b6
John Dibling <john.dibling@picotrading.com>
2015-11-11 14:20:46 -0600

    Implemented cap-packet-recv-sec and (common) packet-recv-time-gmt fields.  Hid other recv fields by default

--- decoder/input/capture/capmsg/types.py
+++ decoder/input/capture/capmsg/types.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 
 class HiResTimeStamp(object):
     def __init__(self, dataField):
@@ -9,9 +10,22 @@ class HiResTimeStamp(object):
         if self.__dataField not in context:
             raise ValueError("Context does not have expected '{0}' field: {1}".format(self.__dataField, context))
         hiRes = float(context[self.__dataField])
-        whole = int(hiRes)
-        mics = int((hiRes-whole)*1000000)
-        whole += self.__epoch
-        dt = datetime.datetime.fromtimestamp(whole).replace(microsecond=mics)
+        hiResGmt = hiRes + self.__epoch
+
+        gmtFrac, gmtWhole = math.modf(hiResGmt)
+        dt = datetime.datetime.fromtimestamp(gmtWhole).replace(microsecond=int(gmtFrac*1000000))
+
         return dt
 
+class HiResRecvSec(object):
+    def __init__(self, dataField):
+        super(HiResRecvSec, self).__init__()
+        self.__dataField = dataField
+        self.__epoch = 1262304000 # seconds from unix epoch to 1/1/2010
+    def __call__(self, context):
+        if self.__dataField not in context:
+            raise ValueError("Context does not have expected '{0}' field: {1}".format(self.__dataField, context))
+        hiRes = float(context[self.__dataField])
+        hiResGmt = hiRes + self.__epoch
+
+        return str(hiResGmt)
